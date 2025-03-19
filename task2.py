2bc7dfe7ad766e40d45d270baf06fe6cf1480a1b
Natalia Pulstenyk <n.pustelnyk@software-inside>
2025-03-19 14:37:27 +0100

    task2

---
+++ task2.py
@@ -0,0 +1,16 @@
+import re
+
+
+def generator_numbers(text: str):
+    pattern = r'\b\d+\.\d+\b'
+    for match in re.findall(pattern, text):
+        yield float(match)
+
+
+def sum_profit(text: str, func):
+    return sum(func(text))
+
+
+text = 'Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів.'
+total_income = sum_profit(text, generator_numbers)
+print(f'Загальний дохід: {total_income}')
